a826e13e32f4eadb4f2418840429819e0c7a8511
Phillip Markert <phillip@liveintent.com>
2020-01-20 16:00:42 +0100

    Added example key generation for dailyplanet.com

--- li_privacy/main/init.py
+++ li_privacy/main/init.py
@@ -79,15 +79,57 @@ class Init(object):
             else:
                 break
 
-        key_id = args.key_id or config.get('key_id','')
-        config['key_id'] = input("Key Identifier: (%s) " % key_id) or key_id
+        example_configuration = config['domain_name']=="dailyplanet.com"
+        if example_configuration:
+            print("Generating example key and configuration")
+            args.config = "dailyplanet.config"
+            key_id = config['key_id'] = "key1"
+            signing_key = config['signing_key'] = config['domain_name'] + ".key"
+            config['staging'] = True
+            config['callback_url'] = ""
 
-        signing_key = args.signing_key or config.get('signing_key', config['domain_name'] + ".key")
-        config['signing_key'] = input(F"Path to Private RSA signing key file: ({signing_key}) ") or signing_key
+            # These example keys are only valid in the staging environment which does not execute live transactions
+            # therefore they are safe to embed in this source. These keys are also documented in our API guide
+            # and are included here for end-user convenience.
+            # For obvious reasons, these keys do not work in the production system. :)
 
-        rsa_key = self.generateKey(config['signing_key'])
+            with open(signing_key, "w+") as private_key:
+                private_key.write("""-----BEGIN RSA PRIVATE KEY-----
+MIICWwIBAAKBgQCXetR4Wz3YxxEZxArubSXHtkACZ9CIPvc7r9AqmfCR4UM+xG5G
+7VMU8KRDZrmEaKUzHWVmRSolDIGPFGXjv+csAzBA2aASI4PkxbeYov7xYFD1lQ4k
+TTeg+bj0UaivNOChFUHMWwe5I/sVh7wcwIA1kJfQ15lJOgwBfz5fP8URKwIDAQAB
+AoGALj0jQD3xygsx8CCEibUtlCHQtitEX2KBC2oma+qjoZQWd8F0PBhThQ/TxHNF
+6+IZk1nEywwPylFf9vHuDDBW+wMg9oErNd6C/KlsVaPth/cQxWU5E5IlaANg5rKA
+4VbisjXDkc0H/UXc+Dka4CGwfbdHm7ZylCXgYKNtlLtqcRECQQDFqBe6rz/B3vsU
+nxyIxPo6BkrVtKhZHoyy+O69qAfy+VpDBnaA3kUWXwvVpvu1QGkdOYMeKJ1j0jYb
+2Ayj3tQpAkEAxDFj5RcLnL3dSVORGUBt3uOOqJc+g2JQMZkbElM+9CZMtvOqghWQ
+LrLuJKaqADkjnNNrhwRZ+49ivRBnJdwFMwJAVFN6jDLoSJYRGKMpUVB4UPkORE5m
+5F6cOF7rvA5MFeU8FQxU0nYBk6HJMsWi7ZklP0qiHePGAihU3Vw3SFJwwQJATKhb
+ttyNTf4lo4wCatJw26EgUaFe7KkSWn7PRBbAx1bbrLSCj/dq8cQ6Jpn0XMf2sUUu
+g3/gxNkepG7vTqysXwJAE23D11+RZW4hM92TC8zohTw/jgEXR/klWMWykMuidc+M
+t1n7m+87k1K1LL11cfK3X4jbdfv0am1EDOtsPIhvzQ==
+-----END RSA PRIVATE KEY-----""")
+            with open(signing_key + ".pub", "w+") as public_key:
+                public_key.write("""-----BEGIN PUBLIC KEY-----
+MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCXetR4Wz3YxxEZxArubSXHtkAC
+Z9CIPvc7r9AqmfCR4UM+xG5G7VMU8KRDZrmEaKUzHWVmRSolDIGPFGXjv+csAzBA
+2aASI4PkxbeYov7xYFD1lQ4kTTeg+bj0UaivNOChFUHMWwe5I/sVh7wcwIA1kJfQ
+15lJOgwBfz5fP8URKwIDAQAB
+-----END PUBLIC KEY-----""")
+            print("Saved example keys in " + signing_key + " and " + signing_key + ".pub")
+            print("To use these example keys, add --config " + args.config + " to your command.")
+        else:
+            key_id = args.key_id or config.get('key_id','')
+            config['key_id'] = input("Key Identifier: (%s) " % key_id) or key_id
+
+            signing_key = args.signing_key or config.get('signing_key', config['domain_name'] + ".key")
+            config['signing_key'] = input(F"Path to Private RSA signing key file: ({signing_key}) ") or signing_key
+
+            rsa_key = self.generateKey(config['signing_key'])
 
         # Write config file
         with open(args.config, "w+") as config_file:
             json.dump(config, config_file, indent=2)
         print(F"Configuration written to {args.config}")
+        if not example_configuration:
+            self.printProvisioningNotice(args, config)
